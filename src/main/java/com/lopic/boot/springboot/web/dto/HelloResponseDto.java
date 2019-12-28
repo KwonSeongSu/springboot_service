e493811bb92b9779601977ee9c31937fac228299
KwonSeongSu <33721669+KwonSeongSu@users.noreply.github.com>
2019-12-29 03:24:19 +0900

    lombok 설치 및 테스트

---
+++ src/main/java/com/lopic/boot/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.lopic.boot.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
