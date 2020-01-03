16df7156c7fc48787380b1128dd4544f425e0024
KwonSeongSu <33721669+KwonSeongSu@users.noreply.github.com>
2020-01-03 11:00:46 +0900

    게시글 조회....

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,33 @@
+var main = {
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function () {
+            _this.save();
+            console.log(this);
+        });
+    },
+    save : function () {
+            var data = {
+                title: $('#title').val(),
+                author: $('#author').val(),
+                content: $('#content').val()
+            };
+
+            console.log(data.title);
+
+            $.ajax({
+                type: 'POST',
+                url: '/api/v1/posts',
+                dataType: 'json',
+                contentType:'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function() {
+                alert('글이 등록되었습니다.');
+                window.location.href = '/';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        }
+};
+
+main.init(); //필요한 값들을 초기화 함수에 init()를  많이 사용합니다.
